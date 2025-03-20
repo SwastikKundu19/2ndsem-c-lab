045617e1a82c6fcc72f38e628275aebd4fb67cf6
Swastik Kundu <swastikscode1@gmail.com>
2025-03-20 09:06:25 +0530

    Delete basic directory

--- basic/1.1.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(int argc, char** argv)
-{
-	printf("Swastik Kundu");
-	return 0;
-}
--- basic/2.4.c
+++
@@ -1,8 +0,0 @@
-//write a simple program that print the result of all the operators available in c (including pre/ post incriment,bitwise and logical.)
-
-#include <stdio.h>
-int main(int argc, char** argv)
-{
-	
-	return 0;
-}
