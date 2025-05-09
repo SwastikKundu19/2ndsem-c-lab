8f1de6a8a6798a7316c6fa063681b7c8f69c3713
Swastik Kundu <swastikscode1@gmail.com>
2025-05-09 12:51:23 +0530

    Add files via upload

---
+++ 02.Variable types and type conversions/2.2.c
@@ -0,0 +1,13 @@
+//wap to add two number.
+
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter 1st number=");
+	scanf("%d",&a);
+	printf("Enter 2nd number=");
+	scanf("%d",&b);
+	printf("Addition of %d and %d = %d",a,b,a+b);
+	return 0;
+}
